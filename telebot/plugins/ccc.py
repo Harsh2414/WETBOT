b42de05533e9fa4d25217d6149781e02208b7dd1
xditya <58950863+xditya@users.noreply.github.com>
2021-01-06 09:00:36 +0000

    pylint: auto fixes

--- telebot/plugins/ccc.py
+++ telebot/plugins/ccc.py
@@ -1,30 +1,33 @@
-from telethon import events
 import asyncio
 
+from telethon import events
 
-@telebot.on(admin_cmd(pattern='(bin|vbv|me|key|iban)(.*)', outgoing=True))
+
+@telebot.on(admin_cmd(pattern="(bin|vbv|me|key|iban)(.*)", outgoing=True))
 async def _(event):
- chat = 'carol5_bot'
- input_str = event.pattern_match.group(1)
- b = event.pattern_match.group(2)
- if input_str == 'bin':
-  a = f'/bin {b}'
- elif input_str == 'vbv':
-  a = f'/vbv {b}'
- elif input_str == 'me':
-  a = f'/me {b}'
- elif input_str == 'key':
-  a = f'/key {b}'
- elif input_str == 'iban':
-  a = f'/iban {b}'
- else:
-  return
-  
- async with event.client.conversation(chat) as conv:
-  response = conv.wait_event(events.NewMessage(incoming=True,from_users=1247032902))
-  await event.client.send_message(chat, '/start')
-  await asyncio.sleep(3)
-  await event.client.send_message(chat, a)
-  response = await response
-  await event.client.send_message(event.chat_id, response.message)
-  await event.delete()
+    chat = "carol5_bot"
+    input_str = event.pattern_match.group(1)
+    b = event.pattern_match.group(2)
+    if input_str == "bin":
+        a = f"/bin {b}"
+    elif input_str == "vbv":
+        a = f"/vbv {b}"
+    elif input_str == "me":
+        a = f"/me {b}"
+    elif input_str == "key":
+        a = f"/key {b}"
+    elif input_str == "iban":
+        a = f"/iban {b}"
+    else:
+        return
+
+    async with event.client.conversation(chat) as conv:
+        response = conv.wait_event(
+            events.NewMessage(incoming=True, from_users=1247032902)
+        )
+        await event.client.send_message(chat, "/start")
+        await asyncio.sleep(3)
+        await event.client.send_message(chat, a)
+        response = await response
+        await event.client.send_message(event.chat_id, response.message)
+        await event.delete()
