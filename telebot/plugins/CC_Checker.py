2ab3788b7a3ed6e2c5f9950a1a09571c162ba5b7
xditya <58950863+xditya@users.noreply.github.com>
2021-02-17 02:44:39 +0000

    pylint: auto fixes

--- telebot/plugins/CC_Checker.py
+++ telebot/plugins/CC_Checker.py
@@ -1,7 +1,6 @@
-from userbot.utils import admin_cmd
-from selenium import webdriver
 import requests
-import html
+from userbot.utils import admin_cmd
+
 
 @borg.on(admin_cmd(pattern="bin ?(.*)"))
 async def _(event):
@@ -12,18 +11,20 @@ async def _(event):
         url = f"https://lookup.binlist.net/{kek}"
         midhunkm = requests.get(url=url).json()
         kekvro = midhunkm["country"]
-        ninja = midhunkm["bank"]
-        data_is = (f"✅ Provided Bin is Valid!\n\n<b>Bin</b>: {kek} \n"
-           f"<b>Brand</b>: {midhunkm['scheme']} \n"
-           f"<b>Level</b>: {midhunkm['brand']} \n"
-           f"<b>Bank</b>: {kekvro['name']} \n"
-           f"<b>Country</b>: {kekvro['name']} {kekvro['emoji']} \n\n"
-           f"🌐<b>Info Provided By Ninja Userbot</b>🌐 \n")
+        midhunkm["bank"]
+        data_is = (
+            f"✅ Provided Bin is Valid!\n\n<b>Bin</b>: {kek} \n"
+            f"<b>Brand</b>: {midhunkm['scheme']} \n"
+            f"<b>Level</b>: {midhunkm['brand']} \n"
+            f"<b>Bank</b>: {kekvro['name']} \n"
+            f"<b>Country</b>: {kekvro['name']} {kekvro['emoji']} \n\n"
+            f"🌐<b>Info Provided By Ninja Userbot</b>🌐 \n"
+        )
         await event.edit(data_is, parse_mode="HTML")
-    except:
+    except BaseException:
         await event.edit("Not a Valid Bin Or Don't Have Enough Info.")
 
-                   
+
 @borg.on(admin_cmd(pattern="iban ?(.*)"))
 async def _(event):
     if event.fwd_from:
@@ -33,14 +34,16 @@ async def _(event):
     lol = requests.get(url=api).json()
     try:
         banks = lol["bankData"]
-        kek = (f"<b>Provided IBAN is Valid:</b>: {lol['valid']} \n"
-           f"<b>IBAN</b>: {lol['iban']} \n"
-           f"<b>BANK-CODE</b>: {banks['bankCode']} \n"
-           f"<b>BANK-NAME</b>: {banks['name']} \n"
-           f"<b>ZIP</b>: {banks['zip']} \n"
-           f"<b>CITY</b>: {banks['city']} \n"
-           f"<b>BIC</b>: {banks['bic']} \n\n"
-         f"🌐<b>Info Provided By Ninja Userbot</b>🌐 \n")
+        kek = (
+            f"<b>Provided IBAN is Valid:</b>: {lol['valid']} \n"
+            f"<b>IBAN</b>: {lol['iban']} \n"
+            f"<b>BANK-CODE</b>: {banks['bankCode']} \n"
+            f"<b>BANK-NAME</b>: {banks['name']} \n"
+            f"<b>ZIP</b>: {banks['zip']} \n"
+            f"<b>CITY</b>: {banks['city']} \n"
+            f"<b>BIC</b>: {banks['bic']} \n\n"
+            f"🌐<b>Info Provided By Ninja Userbot</b>🌐 \n"
+        )
         await event.edit(kek, parse_mode="HTML")
-    except:
+    except BaseException:
         await event.edit(f"Invalid IBAN Or Doesn't Have Enough Info")
